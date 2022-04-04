6757eb376740b0d225605bd8df63a409fd572f9b
Sinqq <100397624+Sinqq@users.noreply.github.com>
2022-04-04 10:34:17 +0530

    smort updates
    updated, should work more efficiently now

--- maths.rb
+++ maths.rb
@@ -1,32 +1,41 @@
 # Made by Sinqq
-print "Wanted (add,subs,divd,mltp):"
+print "Wanted (add,subs,divd,mltp, algb):"
 need = gets.chomp
 if need = "add" then {
   print "Num1:"
   addNum1 = gets.chomp
   print "Num2:"
   addNum2 = gets.chomp
-  puts addNum1 + addNum2 "is your sum"
+  puts "#{addNum1 = addNum2} is your sum"
 }
 if need = "subs" then {
   print "Minuend:"
   subNum1 = gets.chomp
   print "Subtrahend:"
   subNum2 = gets.chomp
-  puts subNum1 - subNum2 "is your difference"
+  puts "#{subNum1 - subNum2} is your difference"
 }
 if need = "divd" then {
   print "dividend:"
   divd = gets.chomp
   print "Divisor:"
   divs = gets.chomp
-  puts divd/divs "is your quotient, no remainder.. yet"
+  puts divd/divs "#{divd / divs}is your quotient, no remainder.. yet"
 }
 if need = "mltp" then {
   print "Number 1"
   mltpNum1 = gets.chomp
   print "Number 2"
   mltpNum2 = gets.chomp
-  puts mltpNum1 * mltpNum2 "is your result"
+  puts 
 }
+  if need = "algb" then {
+    print "Known number:"
+    algbNum1 = gets.chomp
+    print "Unknown variable:"
+    algbVari1 = gets.chomp
+    print "Answer / key:"
+    algbKeys1 = gets.chomp
+    if algbKeys1 > algbNum1 then{
+      puts "#{algbKeys1 - algbNum1} is your answer." 
 end
